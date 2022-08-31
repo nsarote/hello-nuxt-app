394f50f75b1f4b77c10641512835ef32f0005062
Sarote Ruangchai <RuSarote@central.co.th>
2022-08-31 13:37:05 +0700

    - change axios config baseUrl

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,7 +82,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:3000/api',
+    // baseURL: 'http://localhost:12345/api',
+    baseURL: '/api',
   },
 
   auth: {
