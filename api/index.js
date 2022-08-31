91347dabb32486e5ff418296b1482d20264a9a69
Sarote Ruangchai <RuSarote@central.co.th>
2022-08-31 18:07:17 +0700

    Try to fix cors

--- api/index.js
+++ api/index.js
@@ -1,8 +1,22 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  next()
+})
 
 app.get('/hello', (req, res) => {
-  res.json({ message: 'Hello from serverMiddleware', title: 'This is title from api hello' });
-});
+  res.json({
+    message: 'Hello from serverMiddleware',
+    title: 'This is title from api hello',
+  })
+})
 
-module.exports = app;
+module.exports = app
