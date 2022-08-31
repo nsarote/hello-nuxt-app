f06ed2955c5a75d43bcc2cb86be57b00b5814ef9
Sarote Ruangchai <RuSarote@central.co.th>
2022-08-31 14:46:52 +0700

    check env for set baseUrl

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,112 +1,120 @@
-export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: true,
+export default () => {
 
-  mode: 'universal',
+  const isDev = process.env.NODE_ENV === 'development';
+  console.log('NODE_ENV:',process.env.NODE_ENV);
+  const baseUrl = isDev?'':'hello-nuxt-app';
+  console.log('baseUrl:',baseUrl);
 
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
-  // target: 'server',
+  return {
+    // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+    ssr: true,
 
-  serverMiddleware: [
-    { path: '/api', handler: '~/api/index.js' },
-    { path: '/api', handler: '~/api/auth.js' },
-  ],
+    mode: 'universal',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    titleTemplate: 'Hello Nuxt.js - %s',
-    title: process.env.npm_package_name || 'hello-nuxt-app',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_name || 'hello-nuxt-app',
-      },
-      { name: 'format-detection', content: 'telephone=no' },
+    // Target: https://go.nuxtjs.dev/config-target
+    target: 'static',
+    // target: 'server',
+
+    serverMiddleware: [
+      { path: '/api', handler: '~/api/index.js' },
+      { path: '/api', handler: '~/api/auth.js' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/stylelint-module
-    // '@nuxtjs/stylelint-module',
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss',
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-    // https://go.nuxtjs.dev/content
-    '@nuxt/content',
-
-    '@nuxtjs/auth',
-  ],
-
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
-  pwa: {
-    manifest: {
-      lang: 'en',
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+      titleTemplate: 'Hello Nuxt.js - %s',
+      title: process.env.npm_package_name || 'hello-nuxt-app',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: process.env.npm_package_name || 'hello-nuxt-app',
+        },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     },
-  },
 
-  // Content module configuration: https://go.nuxtjs.dev/config-content
-  content: {
-    markdown: {
-      prism: {
-        theme: 'prism-themes/themes/prism-material-oceanic.css',
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+      // https://go.nuxtjs.dev/typescript
+      '@nuxt/typescript-build',
+      // Doc: https://github.com/nuxt-community/eslint-module
+      '@nuxtjs/eslint-module',
+      // Doc: https://github.com/nuxt-community/stylelint-module
+      // '@nuxtjs/stylelint-module',
+      // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+      '@nuxtjs/tailwindcss',
+    ],
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+      // https://go.nuxtjs.dev/axios
+      '@nuxtjs/axios',
+      // https://go.nuxtjs.dev/pwa
+      '@nuxtjs/pwa',
+      // https://go.nuxtjs.dev/content
+      '@nuxt/content',
+
+      '@nuxtjs/auth',
+    ],
+
+    // PWA module configuration: https://go.nuxtjs.dev/pwa
+    pwa: {
+      manifest: {
+        lang: 'en',
       },
     },
-    nestedProperties: ['author.name'],
-  },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    // baseURL: 'http://localhost:12345/api',
-    baseURL: '/api',
-  },
-
-  auth: {
-    strategies: {
-      local: {
-        endpoints: {
-          login: { url: 'login', method: 'post', propertyName: 'data.token' },
-          user: { url: 'me', method: 'get', propertyName: 'data' },
-          logout: false,
+    // Content module configuration: https://go.nuxtjs.dev/config-content
+    content: {
+      markdown: {
+        prism: {
+          theme: 'prism-themes/themes/prism-material-oceanic.css',
         },
       },
+      nestedProperties: ['author.name'],
     },
-    redirect: {
-      login: '/login',
-    },
-  },
 
-  router: {
-    base: process.env.BASE_URL,
-    middleware: ['auth'],
-  },
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {},
+
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {
+      // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+      // baseURL: 'http://localhost:12345/api',
+      baseURL: '/api',
+    },
+
+    auth: {
+      strategies: {
+        local: {
+          endpoints: {
+            login: { url: 'login', method: 'post', propertyName: 'data.token' },
+            user: { url: 'me', method: 'get', propertyName: 'data' },
+            logout: false,
+          },
+        },
+      },
+      redirect: {
+        login: '/login',
+      },
+    },
+
+    router: {
+      base: baseUrl,
+      middleware: ['auth'],
+    },
+  }
 }
